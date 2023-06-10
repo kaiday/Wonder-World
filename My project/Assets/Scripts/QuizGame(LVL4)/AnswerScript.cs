eba539c0501ec6539ed7f33adb728f25d95285ac
kaiday <107856436+kaiday@users.noreply.github.com>
2023-06-10 09:17:20 +0700

    Demo Changes 0.2.1
    Reup La Minh Demo Changes

--- My project/Assets/Scripts/QuizGame(LVL4)/AnswerScript.cs
+++ My project/Assets/Scripts/QuizGame(LVL4)/AnswerScript.cs
@@ -17,7 +17,7 @@ public class AnswerScript : MonoBehaviour
         else
         {
             Debug.Log("is False");
-            quizManager.correct();
+            quizManager.wrong();
         }
     }
 }
--- My project/Assets/Scripts/QuizGame(LVL4)/QuizManager.cs
+++ My project/Assets/Scripts/QuizGame(LVL4)/QuizManager.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class QuizManager : MonoBehaviour
 {
@@ -9,14 +11,53 @@ public class QuizManager : MonoBehaviour
     public GameObject[] options;
     public int currentQuestion;
 
+    public GameObject QuizPanel;
+    public GameObject OverPanel;
+    
     public Text QuestionTxt;
+    public Text ScoreTxt;
+    public Text QuizText;
+    public Image Ranking;
+
+    public int totalQuestion;
+    public int scoreCount;
+    public int quizCount;
+
+    public Sprite Fail;
+    public Sprite Bronze;
+    public Sprite Silver;
+    public Sprite Gold;
 
     private void Start()
     {
+        totalQuestion = QnA.Count;
+        OverPanel.SetActive(false);
         generateQuestion();
     }
 
+    public void retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public void GameOver()
+    {
+        QuizPanel.SetActive(false);
+        OverPanel.SetActive(true);
+        compareMedal();
+        ScoreTxt.text = scoreCount + "PTS";
+        QuizText.text = quizCount + "/" + totalQuestion;
+    }
+    
     public void correct()
+    {
+        scoreCount += 15;
+        quizCount += 1;
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    public void wrong()
     {
         QnA.RemoveAt(currentQuestion);
         generateQuestion();
@@ -48,6 +89,39 @@ public class QuizManager : MonoBehaviour
         else
         {
             Debug.Log("Out of Question");
+            GameOver();
+        }
+        
+    }
+
+    void compareMedal()
+    {
+        switch (quizCount)
+        {
+            case 1:  
+            {
+                Debug.Log("You got Bronze!");
+                Ranking.GetComponent<Image>().sprite = Bronze;
+                break;
+            } 
+            case 2:
+            {
+                Debug.Log("You got Silver!");
+                Ranking.GetComponent<Image>().sprite = Silver;
+                break;
+            }
+            case 3:
+            {
+                Debug.Log("You got Gold!");
+                Ranking.GetComponent<Image>().sprite = Gold;
+                break;
+            }
+            default:
+            {
+                Debug.Log("You failed!");
+                Ranking.GetComponent<Image>().sprite = Fail;
+                break;
+            }
         }
         
     }
