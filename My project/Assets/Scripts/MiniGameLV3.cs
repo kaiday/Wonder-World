ea82e3a4925e8bcbf4c7db4ba3a33c0ffc46d667
kaiday <107856436+kaiday@users.noreply.github.com>
2023-06-11 21:32:28 +0700

    Demo 0.3.3
    Stuck with changing scene  and destroying portal after finishing mingame

--- My project/Assets/Scripts/MiniGameLV3.cs
+++ My project/Assets/Scripts/MiniGameLV3.cs
@@ -4,40 +4,58 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+
 public class MiniGameLV3 : MonoBehaviour
 {
 
     private AudioSource finishSound;
-
-
-    private int count = 0;
+    public string m_Scene = "Level 1";
+    public GameObject m_MyGameObject;
 
 
     private void Start()
     {
         finishSound = GetComponent<AudioSource>();
+       
     }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Animal")
+        if (collision.gameObject.tag == "Rhino")
         {
-            count++;
-            Debug.Log(count);
 
-            if(count == 13)
-            {
-                finishSound.Play();
-                
-                Invoke("CompleteLevel", 0.75f);
-            }
+            finishSound.Play();
 
+            ScriptLV1.portal = false;
+
+            StartCoroutine(LoadYourAsyncScene());
         }
     }
 
-    private void CompleteLevel()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+ 
 
+    IEnumerator LoadYourAsyncScene()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(m_Scene, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+        
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(m_MyGameObject, SceneManager.GetSceneByName(m_Scene));
+        ScriptLV1.portal = false;
+        
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+    }
 }
+
+
