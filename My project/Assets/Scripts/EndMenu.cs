412f82a4b00a7d166b21cb71cfcff4a74806ac4d
NobleMode <laminhcokho@gmail.com>
2023-09-14 21:23:16 +0700

    Demo 1.8.1
    Update small misc item

--- My project/Assets/Scripts/EndMenu.cs
+++ My project/Assets/Scripts/EndMenu.cs
@@ -1,10 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene(0);
     }
 }
